6f11c4528c12576fff9e2da4a60acac36237a0ae
19NikaV04 <150028682+19NikaV04@users.noreply.github.com>
2024-09-05 23:58:29 +0300

    Add files via upload

---
+++ MarketBehaviour.java
@@ -0,0 +1,9 @@
+package Урок_4;
+
+
+public interface MarketBehaviour {
+
+    void acceptOrder(String order);
+    String fulfillOrder();
+}
+
---
+++ QueueBehaviour.java
@@ -0,0 +1,9 @@
+package Урок_4;
+
+
+public interface QueueBehaviour {
+    
+    void enqueue(String person);
+    String dequeue();
+    int size();
+}
